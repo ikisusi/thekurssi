a383e0818cbe065acf550219950df1cdd947f649
Ossi Herrala <oherrala@iki.fi>
2017-09-14 15:40:04 +0300

    This thing can already fuzz stuff

---
+++ labs/fuzzing2-librsvg/fuzz.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# LeakSanitizer is spammy with librsvg
+export ASAN_OPTIONS='detect_leaks=0'
+
+for fuzzcase in tests/*.svg; do
+    ./librsvg/rsvg-convert -o /dev/null "${fuzzcase}"
+    if [ $? -gt 1 ]; then
+        echo "Test case was ${fuzzcase}"
+        exit 1
+    fi
+done
