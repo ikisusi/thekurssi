aeec664a5464bd4565d3324ddd22902d542f29db
Ossi Herrala <oherrala@iki.fi>
2017-09-14 18:34:33 +0300

    Compile and install librsvg from upstream

--- labs/fuzzing2-librsvg/fuzz.sh
+++ labs/fuzzing2-librsvg/fuzz.sh
@@ -4,7 +4,7 @@
 export ASAN_OPTIONS='detect_leaks=0'
 
 for fuzzcase in tests/*.svg; do
-    ./librsvg/rsvg-convert -o /dev/null "${fuzzcase}"
+    /app/bin/rsvg-convert -o /dev/null "${fuzzcase}"
     if [ $? -gt 1 ]; then
         echo "Test case was ${fuzzcase}"
         exit 1
