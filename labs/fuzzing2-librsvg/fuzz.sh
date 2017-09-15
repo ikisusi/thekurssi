f181f0cc8d343f62b63fc700c496311426412c77
Ossi Herrala <oherrala@iki.fi>
2017-09-15 20:06:24 +0300

    Collect samples that trigger ASAN.

--- labs/fuzzing2-librsvg/fuzz.sh
+++ labs/fuzzing2-librsvg/fuzz.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 # LeakSanitizer is spammy with librsvg
-export ASAN_OPTIONS='detect_leaks=0'
+export ASAN_OPTIONS='detect_leaks=0:exitcode=127'
 
+mkdir -p /work/crashes
 for fuzzcase in tests/*.svg; do
     /app/bin/rsvg-convert -o /dev/null "${fuzzcase}"
-    if [ $? -gt 1 ]; then
+    if [ $? -eq 127 ]; then
         echo "Test case was ${fuzzcase}"
-        exit 1
+        SHA=$(sha1sum "${fuzzcase}" | cut -d' ' -f1)
+        cp "${fuzzcase}" "/work/crashes/${SHA}"
+        /app/bin/rsvg-convert -o /dev/null "${fuzzcase}" 2>&1 | tee "/work/crashes/${SHA}.txt"
     fi
 done
